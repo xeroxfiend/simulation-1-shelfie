70e1bcf6d78928c19cb269f30d6604cbb3083c5c
Brian Lowe <mynameisboy@gmail.com>
2019-09-13 09:43:58 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//set up server and dependencies
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const massive = require("massive");
+
+app.use(express.json());
+
+//endpoints
+
+// app.get
+
+// app.post
+
+// app.put
+
+// app.delete
+
+massive(CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("connected to the database, and ");
+  app
+    .listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}!`))
+}).catch(err => console.log(err));
+
